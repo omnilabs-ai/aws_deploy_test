8a28eb96c870ba163b1e64e953433cbcbebcc5c8
satyashah <88096065+satyashah@users.noreply.github.com>
2025-02-20 14:27:02 -0500

    trying vercel

--- api/main.py
+++ api/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from mangum import Mangum
-
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
-handler = Mangum(app=app)
 
 # uvicorn api.main:app --reload
